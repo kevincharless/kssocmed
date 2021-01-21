bc17d382fb59771fe38680b80491cd400ec35750
Kevin Charles <65108823+kevincharless@users.noreply.github.com>
2021-01-21 11:07:03 +0700

    Update user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -15,7 +15,7 @@ export const signin = async (req, res) => {
 
         if(!isPasswordCorrect) return res.status(400).json({ message: "Password is incorrect" });
 
-        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'JWTSecret', { expiresIn: '1h' });
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, { expiresIn: '1h' });
 
         res.status(200).json({ result: existingUser, token });
     } catch (error) {
