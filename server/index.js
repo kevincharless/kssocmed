d5f54dae135e68bd36609a9fb4f8cd9d3e0380eb
Kevin Charles <kevinismage@gmail.com>
2021-01-21 10:51:50 +0700

    Setup Server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import userRoutes from './routes/userRoutes.js';
+
+const app = express();
+
+dotenv.config();
+
+
+// BodyParser Middleware
+app.use(express.json());
+
+// Routes
+app.use('/user', userRoutes);
+
+// Server Port
+const PORT = process.env.PORT || 5000
+
+// Connect to Mongodb
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+    .catch(error => console.log(error.message))
+
+mongoose.set('useFindAndModify', false);
