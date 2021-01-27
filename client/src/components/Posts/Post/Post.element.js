661450c9c132a61264db9db406d9db39238bbcff
Kevin Charles <kevinismage@gmail.com>
2021-01-27 19:15:21 +0700

    Card Header

--- client/src/components/Posts/Post/Post.element.js
+++ client/src/components/Posts/Post/Post.element.js
@@ -15,3 +15,37 @@ export const Card = styled.div`
         height: 16rem;
     }
 `
+
+export const Header = styled.div`
+    display: grid;
+    display: flex;
+    grid-row: auto;
+    padding: 1.2rem;
+    align-items: center;
+    justify-content: space-between;
+    
+`
+
+export const Avatar = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+export const AvatarPicture = styled.img`
+    width: 2.2rem;
+    height: 2.2rem;
+    border-radius: 80px;
+    margin-right: 0.8rem;
+    
+    display: block;
+`
+
+export const AvatarName = styled.h3`
+    font-style: normal;
+    font-weight: 500;
+    font-size: 0.9rem;
+    text-align: center;
+
+    display: block;
+    color: #176D84;
+`
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Card } from './Post.element';
+import { RiMoreFill } from 'react-icons/ri';
+
+import { Card, Header, Avatar, AvatarPicture, AvatarName } from './Post.element';
+import kevin from '../../../assets/images/twibonhmps.jpg'
 
 const Post = () => {
     return (
         <Card>
-            test
+            <Header>
+                <Avatar>
+                    <AvatarPicture src={kevin} />
+                    <AvatarName>Kevin Charles</AvatarName>
+                </Avatar>
+                <RiMoreFill style={{fontSize: '1.5rem', color: ' #176D84', cursor: 'pointer'}} />
+            </Header>
         </Card>
     )
 }
