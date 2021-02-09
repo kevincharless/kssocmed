b84e6139d26d7b8410a4375a6cca558951d4d231
Kevin Charles <kevinismage@gmail.com>
2021-02-09 13:26:48 +0700

    Fix mutiple key bug in comment

--- client/src/components/Comments/Comments.js
+++ client/src/components/Comments/Comments.js
@@ -5,8 +5,8 @@ import CommentInput from './CommentInput/CommentInput';
 const Comments = ({ post, user }) => {
     return (
         <>
-            {post.comments.map(comment => (
-                <Comment key={post._id} comment={comment} />
+            {post.comments.map((comment, i) => (
+                <Comment key={i} comment={comment} />
             ))}
             <CommentInput post={post} user={user} />
         </>
